51580a6bfe49fea183e4e3e7bf8b570eeaad8f67
PRyCpl <PRyCpl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-16 07:04:57 +0000

    First public ver.
    git-svn-id: https://plugins.svn.wordpress.org/pryc-woocommerce-product-search-in-any-search/trunk@949416 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pryc-woocommerce-product-search-in-any-search.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Plugin Name: PRyC WooCommerce product search in any search
+ * Plugin URI: http://PRyC.pl
+ * Description: Search WooCommerce product at default theme search field or any other...
+ * Author: PRyC
+ * Author URI: http://PRyC.pl
+ * Version: 1.0.1
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+ 
+/* Copyright PRyC (email: kontakt@PRyC.pl)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+ 
+ 
+/* CODE: */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+function pryc_woocommerce_product_search_in_any_search() {
+        if ( is_search() && !empty($_GET['s']) && empty($_GET['post_type'])) {
+                wp_redirect(home_url("/?s=") . urlencode(get_query_var('s')) . "&post_type=product");
+                exit();
+        }
+}
+add_action('template_redirect', 'pryc_woocommerce_product_search_in_any_search');
+
+/* END */
+?>
---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== Plugin Name ===
+Contributors: PRyCpl
+Donate link: http://pryc.pl/
+Tags: WooCommerce, search, search field, ecommerce
+Requires at least: 3.0.1
+Tested up to: 3.9
+Stable tag: 1.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Search WooCommerce product at default theme search field or any other...
+
+== Description ==
+
+Search WooCommerce product at default theme search field or any other...
+
+
+== Installation ==
+
+1. Upload `pryc-woocommerce-product-search-in-any-search` dir to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+== Changelog ==
+
+= 1.0/1 =
+* First public ver.
+
+== Arbitrary section ==
+
+== A brief Markdown Example ==
