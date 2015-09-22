0e526227dee21e633755fd27b8c4d51e67298f87
PRyCpl <PRyCpl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-22 05:45:40 +0000

    some fix
    git-svn-id: https://plugins.svn.wordpress.org/pryc-woocommerce-product-search-in-any-search/trunk@1250914 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pryc-woocommerce-product-search-in-any-search.php
+++ pryc-woocommerce-product-search-in-any-search.php
@@ -5,7 +5,7 @@
  * Description: Search WooCommerce product at default theme search field or any other...
  * Author: PRyC
  * Author URI: http://PRyC.pl
- * Version: 1.0.4
+ * Version: 1.0.5
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
@@ -32,11 +32,13 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-function pryc_woocommerce_product_search_in_any_search() {
-        if ( is_search() && !empty($_GET['s']) && empty($_GET['post_type'])) {
-				wp_safe_redirect("/?s=" . urlencode(get_query_var('s')) . "&post_type=product");
-                exit();
-        }
+if ( !function_exists("pryc_woocommerce_product_search_in_any_search") ) {
+	function pryc_woocommerce_product_search_in_any_search() {
+			if ( is_search() && !empty($_GET['s']) && empty($_GET['post_type'])) {
+					wp_safe_redirect("/?s=" . urlencode(get_query_var('s')) . "&post_type=product");
+					exit();
+			}
+	}
 }
 add_action('template_redirect', 'pryc_woocommerce_product_search_in_any_search');
 
